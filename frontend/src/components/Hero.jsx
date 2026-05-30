609448670d17b7f776891df60a27deac2e425bd3
Pushkar0730 <pushkar.bhegade18@gmail.com>
2026-05-30 16:48:42 +0530

    Switch to HashRouter

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import { useEffect } from "react"
 import { useState } from "react"
-import products from ".."
 
 export default function Hero() {
   return (
